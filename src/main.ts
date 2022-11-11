82960e52bc636161daa74e8bada7ae2215dc5d85
João Alberto <hey@joaoalberto.dev>
2022-11-11 14:47:21 -0300

    adding missing code

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-const main = true;
+import "./style.css";
 
-export { main };
+import * as THREE from "three";
+
+const app = document.querySelector("#app");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  30,
+  window.innerWidth / window.innerHeight,
+  0.2,
+  100
+);
+const renderer = new THREE.WebGL1Renderer();
+const geometry = new THREE.CylinderGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: "#FEBE8C",
+  wireframe: true,
+});
+const cylinder = new THREE.Mesh(geometry, material);
+
+camera.position.z = 10;
+renderer.setSize(window.innerWidth, window.innerHeight);
+app?.appendChild(renderer.domElement);
+scene.add(cylinder);
+
+const animate = () => {
+  cylinder.rotation.x += 0.01;
+  cylinder.rotation.y += 0.01;
+  cylinder.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
+};
+
+animate();
