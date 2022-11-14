e404d212821163468aa37262ca9e7d06e71e6af2
João Alberto <hey@joaoalberto.dev>
2022-11-14 17:36:12 -0300

    feat: adding tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,29 @@
+import {
+  Outlet,
+  RouterProvider,
+  createReactRouter,
+  createRouteConfig,
+} from "@tanstack/react-router";
+
 import "./App.css";
+import Home from "./routes/home";
 
-import { useRef, useState } from "react";
-import { Canvas, ThreeElements, useFrame } from "@react-three/fiber";
+const routeConfig = createRouteConfig().createChildren((createRoute) => [
+  createRoute({
+    path: "/",
+    element: <Home />,
+  }),
+]);
 
-function Cylinder(props: ThreeElements["mesh"]) {
-  const mesh = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [sides, setSides] = useState(3);
-  const [speed, setSpeed] = useState(1);
-
-  useFrame(() =>
-    hovered
-      ? (mesh.current.rotation.x -= 0.01 * speed)
-      : (mesh.current.rotation.x += 0.01 * speed)
-  );
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={1}
-      onClick={() => {
-        setSides((current) => (current += 1));
-        setSpeed((current) => (current += 0.5));
-      }}
-      onPointerOver={() => setHover(true)}
-      onPointerOut={() => setHover(false)}
-    >
-      <cylinderBufferGeometry args={[1, 1, 1, sides]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
+const router = createReactRouter({
+  routeConfig,
+});
 
 function App() {
   return (
-    <Canvas style={{ width: window.innerWidth, height: window.innerHeight }}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Cylinder position={[-1.2, 0, 0]} />
-      <Cylinder position={[1.2, 0, 0]} />
-    </Canvas>
+    <RouterProvider router={router}>
+      <Outlet />
+    </RouterProvider>
   );
 }
 
---
+++ src/routes/home.tsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+function Cylinder(props: any) {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [sides, setSides] = useState(3);
+  const [speed, setSpeed] = useState(1);
+
+  useFrame(() =>
+    hovered
+      ? (mesh.current.rotation.x -= 0.01 * speed)
+      : (mesh.current.rotation.x += 0.01 * speed)
+  );
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={1}
+      onClick={() => {
+        setSides((current) => (current += 1));
+        setSpeed((current) => (current += 0.5));
+      }}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <cylinderBufferGeometry args={[1, 1, 1, sides]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+}
+
+function Home() {
+  return (
+    <Canvas style={{ width: window.innerWidth, height: window.innerHeight }}>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Cylinder position={[-1.2, 0, 0]} />
+      <Cylinder position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+}
+
+export default Home;
