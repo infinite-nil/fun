3752b2c3d285f712b847d4f3feb4637f6fc4d054
João Alberto <hey@joaoalberto.dev>
2022-11-17 22:09:07 -0300

    fix: rendering background as a second layer to increase the performance

--- src/routes/collision.tsx
+++ src/routes/collision.tsx
@@ -22,6 +22,7 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 const OFFSET = 120;
 const INITIAL = (HEIGHT - OFFSET) / 2 - 1;
+const FPS_60 = 1000 / 60;
 
 const solids: Solid<any>[] = [
   {
@@ -53,6 +54,7 @@ const solids: Solid<any>[] = [
 
 const Collision = () => {
   const position = useRef(INITIAL);
+  const backgroundCanvas = useRef<HTMLCanvasElement>(null);
   const canvas = useRef<HTMLCanvasElement>(null);
 
   function getSquareLimit(solid: Solid<"square">, position: number) {
@@ -82,21 +84,23 @@ const Collision = () => {
     return 0;
   }
 
-  function draw(canvas: CanvasRenderingContext2D, y: number = INITIAL) {
-    canvas.clearRect(0, 0, WIDTH, HEIGHT);
-
+  function drawBackground(backgroundCanvas: CanvasRenderingContext2D) {
     solids.forEach((solid) => {
       if (solid.type === "square") {
-        canvas.fillStyle = "rgb(0,0,200)";
-        canvas.fillRect(solid.x, solid.y, solid.rect.w, solid.rect.h);
+        backgroundCanvas.fillStyle = "rgb(0,0,200)";
+        backgroundCanvas.fillRect(solid.x, solid.y, solid.rect.w, solid.rect.h);
       }
 
       if (solid.type === "circle") {
-        canvas.fillStyle = "rgb(0,100,200)";
-        canvas.arc(solid.x, solid.y, solid.rect.r, 0, 2 * Math.PI);
-        canvas.fill();
+        backgroundCanvas.fillStyle = "rgb(0,100,200)";
+        backgroundCanvas.arc(solid.x, solid.y, solid.rect.r, 0, 2 * Math.PI);
+        backgroundCanvas.fill();
       }
     });
+  }
+
+  function draw(canvas: CanvasRenderingContext2D, y: number = INITIAL) {
+    canvas.clearRect(0, 0, WIDTH, HEIGHT);
 
     const position = y - OFFSET / 2;
     canvas.fillStyle = "rgb(200, 0, 0)";
@@ -104,7 +108,12 @@ const Collision = () => {
   }
 
   useEffect(() => {
+    const backgroundContext = backgroundCanvas.current?.getContext("2d");
     const context = canvas.current?.getContext("2d");
+
+    if (backgroundContext) {
+      drawBackground(backgroundContext);
+    }
 
     if (context) {
       document.addEventListener("mousemove", (event) => {
@@ -112,24 +121,42 @@ const Collision = () => {
       });
 
       setInterval(() => {
-        console.time("Draw");
+        console.time("draw");
         draw(context, position.current);
-        console.timeEnd("Draw");
-      }, 8.333);
+        console.timeEnd("draw");
+      }, FPS_60);
     }
   }, [canvas.current]);
 
   return (
-    <>
+    <div
+      style={{
+        position: "relative",
+        width: `${WIDTH - OFFSET}px`,
+        height: `${HEIGHT - OFFSET}px`,
+      }}
+    >
       <canvas
-        ref={canvas}
+        ref={backgroundCanvas}
         style={{
+          position: "absolute",
+          inset: 0,
           margin: `${OFFSET / 2}PX`,
         }}
         width={WIDTH - OFFSET}
         height={HEIGHT - OFFSET}
       ></canvas>
-    </>
+      <canvas
+        ref={canvas}
+        style={{
+          position: "absolute",
+          inset: 0,
+          margin: `${OFFSET / 2}PX`,
+        }}
+        width={WIDTH - OFFSET}
+        height={HEIGHT - OFFSET}
+      ></canvas>
+    </div>
   );
 };
 
